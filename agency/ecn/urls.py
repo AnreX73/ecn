ee0b06c9769b9a6848107abd33afb7712559cdf7
AnreX73 <han220173@gmail.com>
2022-12-30 18:29:05 +0700

    users

--- agency/ecn/urls.py
+++ agency/ecn/urls.py
@@ -1,9 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 
 from ecn.views import *
 
 urlpatterns = [
     path('', index, name='home'),
+    path('about/', about, name='about'),
+]
 
+urlpatterns += [
+    path('users/', include('django.contrib.auth.urls')),
+    path('register/', Register.as_view(), name='register'),
 
 ]
--- agency/ecn/views.py
+++ agency/ecn/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views import View
+from django.contrib.auth.forms import UserCreationForm
 
 
 def index(request):
@@ -7,3 +9,21 @@ def index(request):
 
     }
     return render(request, 'ecn/index.html', context=context)
+
+
+def about(request):
+    context = {
+        'title': 'О нас',
+
+    }
+    return render(request, 'ecn/about.html', context=context)
+
+
+class Register(View):
+    template_name = 'registration/register.html'
+    def get(self, request):
+        context = {
+            'form': UserCreationForm(),
+            'title': 'регистрация',
+        }
+        return render(request, self.template_name, context)
