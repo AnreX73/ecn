a74a0aa32e60c48fdc3a9f8e6e12d751a30ef43c
AnreX73 <han220173@gmail.com>
2023-01-14 15:27:53 +0700

    models start

--- agency/agency/settings.py
+++ agency/agency/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ckeditor',
+    'ckeditor_uploader',
     'ecn.apps.EcnConfig',
 ]
 
@@ -125,9 +127,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = []
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'ecn/media/'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+CKEDITOR_UPLOAD_PATH = "uploads/"
--- agency/ecn/models.py
+++ agency/ecn/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from ckeditor.fields import RichTextField
+from django.urls import reverse
 
 
 class User(AbstractUser):
